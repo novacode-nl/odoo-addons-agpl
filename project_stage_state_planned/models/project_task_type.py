127ed89d4018599283fffcc609d7217a58548da9
Bob Leers <bob@novacode.nl>
2018-02-22 23:42:32 +0100

    project_stage_state_planned: Need import api.

--- project_stage_state_planned/models/project_task_type.py
+++ project_stage_state_planned/models/project_task_type.py
@@ -2,7 +2,7 @@
 # Copyright 2018 Nova Code (http://www.novacode.nl)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import models, fields
+from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
 
