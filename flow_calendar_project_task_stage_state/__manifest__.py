2d9640dec03d9eda8eb87818099ff3859466d0ec
Bob Leers <bob@novacode.nl>
2018-02-22 23:44:49 +0100

    flow_calendar_project_task_stage_state: Set Task to Planned on first (flow)calendar event.

--- flow_calendar_project_task_stage_state/__manifest__.py
+++ flow_calendar_project_task_stage_state/__manifest__.py
@@ -3,8 +3,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': 'Flow Calendar Project Task - Filter Tasks by Stage State',
-    'summary': 'Filter Tasks in the Calendar Event (form).',
+    'name': 'Flow Calendar Project Task - Stage State',
+    'summary': 'Handle Tasks with Stage State in the Calendar Event (form).',
     'description': 'Filter/exclude Tasks, marked done or cancelled, from the Project-Task field in the Calendar Event (form).',
     'version': '0.1',
     'author': 'Nova Code',
@@ -14,8 +14,7 @@
     'depends': [
         # https://apps.odoo.com/apps/modules/10.0/flow_calendar_project_task/
         'flow_calendar_project_task',
-        # https://github.com/OCA/project/tree/10.0/project_stage_state
-        'project_stage_state'
+        'project_stage_state_planned'
     ],
     'data': [],
     'images': [
--- flow_calendar_project_task_stage_state/models/__init__.py
+++ flow_calendar_project_task_stage_state/models/__init__.py
@@ -2,4 +2,6 @@
 # Copyright 2018 Nova Code (http://www.novacode.nl)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-import calendar
+
+from . import calendar
+from . import project
