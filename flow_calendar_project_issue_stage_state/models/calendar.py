c015d6f2a52d30c5d08844f3baa935b0f54ef20b
Bob Leers <bob@novacode.nl>
2018-02-23 11:30:57 +0100

    License (c) year copy/paste mistake :P

--- flow_calendar_project_issue_stage_state/models/calendar.py
+++ flow_calendar_project_issue_stage_state/models/calendar.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Nova Code (http://www.novacode.nl)
+# Copyright 2018 Nova Code (http://www.novacode.nl)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo import api, fields, models, _
