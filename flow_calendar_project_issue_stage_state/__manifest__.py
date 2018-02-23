627db40aaf9a48a60902fc0731c0a3e2f8756059
Bob Leers <bob@novacode.nl>
2018-02-23 09:26:44 +0100

    flow_calendar_project_issue_stage_state: Set Issue to Planned on first (flow)calendar event.

--- flow_calendar_project_issue_stage_state/__manifest__.py
+++ flow_calendar_project_issue_stage_state/__manifest__.py
@@ -3,19 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': 'Flow Calendar Project Issue - Filter Issues by Stage State',
-    'summary': 'Filter Issues in the Calendar Event (form).',
-    'description': 'Filter/exclude Issues, marked done or cancelled, from the Project-Issue field in the Calendar Event (form).',
+    'name': 'Flow Calendar Project Issue - Stage State',
+    'summary': 'Issue planning/calendar features by Stage State.',
     'version': '0.1',
     'author': 'Nova Code',
     'website': 'https://www.novacode.nl',
     'license': 'AGPL-3',
-    'category': 'Extra Tools',
+    'category': 'Project',
     'depends': [
         # https://apps.odoo.com/apps/modules/10.0/flow_calendar_project_issue/
         'flow_calendar_project_issue',
-        # https://github.com/OCA/project/tree/10.0/project_stage_state
-        'project_stage_state'
+        'project_stage_state_planned'
     ],
     'data': [],
     'images': [
