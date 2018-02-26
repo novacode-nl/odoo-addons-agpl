2e33faae56cb3be54dcc5015db7a9f790aaabb6c
Bob Leers <bob@novacode.nl>
2018-02-26 13:29:00 +0100

    [FIX] flow_calendar_project_task_stage_state: Stage planning by create (not always compute!)

--- flow_calendar_project_task_stage_state/models/calendar.py
+++ flow_calendar_project_task_stage_state/models/calendar.py
@@ -12,3 +12,13 @@ class Meeting(models.Model):
         'project.task',
         domain="[('project_id', '=', flowcal_task_project_id), ('stage_id.state', 'not in', ['done', 'cancelled'])]")
     flowcal_task_stage_state = fields.Selection(related='flowcal_task_id.stage_id.state', string='Task Stage State')
+
+    @api.model
+    def create(self, vals):
+        if 'flowcal_task_id' in vals:
+            task = self.env['project.task'].browse(vals.get('flowcal_task_id'))
+            if not task.flowcal_event_ids:
+                planned_task_type = task.project_id.type_ids.filtered(lambda r: r.state == 'planned')
+                task.stage_id = planned_task_type.id
+
+        return super(Meeting, self).create(vals)
