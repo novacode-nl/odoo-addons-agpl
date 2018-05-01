617ce48b917ec8b07738b9c715443f8676e05e0c
Bob Leers <bob@novacode.nl>
2018-05-01 21:48:27 +0200

    project_task_deadline_search_filters: Add new module.

---
+++ project_task_deadline_search_filters/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Nova Code (http://www.novacode.nl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Project Task - Deadline Search Filters',
+    'summary': 'Filter Project Tasks by Deadline before/after today, next weeks(s).',
+    'version': '1.0',
+    'author': 'Nova Code',
+    'website': 'https://www.novacode.nl',
+    'license': 'AGPL-3',
+    'category': 'Project Management',
+    'depends': [
+        'project'
+    ],
+    'data': [
+        'views/project_task_views.xml'
+    ]
+}
