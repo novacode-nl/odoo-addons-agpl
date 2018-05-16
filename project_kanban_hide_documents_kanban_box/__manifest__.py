5dcdaedd6c301b36c5a44bacfcab2b9a5bb10f3f
Bob Leers <bob@novacode.nl>
2018-05-16 20:01:24 +0200

    project_kanban_hide_documents_kanban_box: Add module.

---
+++ project_kanban_hide_documents_kanban_box/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Nova Code (http://www.novacode.nl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Project - Kanban view - Hide Documents kanban box',
+    'summary': 'Hide Documents kanban box in Project kanban view',
+    'version': '1.0',
+    'author': 'Nova Code',
+    'website': 'https://www.novacode.nl',
+    'license': 'AGPL-3',
+    'category': 'Project Management',
+    'depends': [
+        'project'
+    ],
+    'data': [
+        'views/project_views.xml'
+    ]
+}
